644df9b078e51813feda47f1828cab0602aee017
RajiAmma <93925621+RajiAmma@users.noreply.github.com>
2022-02-09 18:26:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var song="";
+leftWristX=0;
+rightWristX=0;
+leftWristY=0;
+rightWristY=0;
 function preload(){
     song=loadSound("05 THIRUPPAVAI.mp3");
 }
@@ -7,10 +11,28 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    posenet=ml5.poseNet(video, modelLoaded);
+    posenet.on('pose' ,gotPoses);
 }
 function draw(){
     image(video,0,0,600,500);
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function modelLoaded(){
+    console.log("Model is Loaded.");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("left wrist X= " +leftWristX+", Left Wrist Y= "+leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("right wrist X= " +rightWristX+", right Wrist Y= "+rightWristY);
+    }
 }
